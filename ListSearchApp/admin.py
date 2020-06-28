e7a867b820434cc86b1753f929359a957485f501
GuysRobot <bluexker@gmail.com>
2020-06-28 13:33:57 -0400

    Init and create first app

---
+++ ListSearchApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from ListSearchApp import models
+
+admin.site.register(models.Search)
+
---
+++ ListSearchApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ListsearchappConfig(AppConfig):
+    name = 'ListSearchApp'
